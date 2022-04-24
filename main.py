ad1ff2b60fe60ac3d7048a4cb9c5b8db69ae06af
mth9406 <mth9406@naver.com>
2022-04-24 17:39:32 +0900

    modified computing sim

--- main.py
+++ main.py
@@ -223,7 +223,7 @@ def trainForEpoch(train_loader, model, optimizer, epoch, num_epochs, criterion, 
         outputs, emb, mask = model(seq)
         sim = torch.matmul(emb, emb.transpose(2,1)) # bs, item_len, item_len
         sim = torch.masked_fill(sim, (mask==0), 1e-8)
-        norms = torch.norm(sim, p=2, dim=-1)
+        norms = torch.norm(emb, p=2, dim=-1)
         norms = torch.unsqueeze(norms, -1)@torch.unsqueeze(norms, 1)
         sim = sim / (norms + 1e-8)
         loss = criterion(outputs, target) - 0.1 * torch.norm(sim)
